692d78079380d4310ad683b62146e5e504ab0504
Nazar96 <43029868+Nazar96@users.noreply.github.com>
2022-09-08 09:07:16 +0200

    update

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-unzip FGT_data.zip
+
+apt install unzip -y
+pip install cupy-cuda11x -f https://pip.cupy.dev/aarch64
+
+#unzip FGT_data.zip
 unzip weights.zip
 mkdir FGT/checkpoint
 mkdir FGT/flowCheckPoint
@@ -8,5 +12,5 @@ mv weights/fgt/* FGT/checkpoint
 mv weights/lafc/* LAFC/checkpoint
 mv weights/lafc_single/* FGT/flowCheckPoint
 rm -r weights
-rm FGT_data.zip
+#rm FGT_data.zip
 rm weights.zip
